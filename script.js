18b3e623a610d87a4e6aeb9594b030f25775e4ce
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-08-03 15:37:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,269 +1,39 @@
-"use strict";
-var UserStatus;
-(function (UserStatus) {
-    UserStatus["LoggedIn"] = "Logged In";
-    UserStatus["LoggingIn"] = "Logging In";
-    UserStatus["LoggedOut"] = "Logged Out";
-    UserStatus["LogInError"] = "Log In Error";
-    UserStatus["VerifyingLogIn"] = "Verifying Log In";
-})(UserStatus || (UserStatus = {}));
-var Default;
-(function (Default) {
-    Default["PIN"] = "0000";
-})(Default || (Default = {}));
-var WeatherType;
-(function (WeatherType) {
-    WeatherType["Cloudy"] = "Cloudy";
-    WeatherType["Rainy"] = "Rainy";
-    WeatherType["Stormy"] = "Stormy";
-    WeatherType["Sunny"] = "Sunny";
-})(WeatherType || (WeatherType = {}));
-const defaultPosition = () => ({
-    left: 0,
-    x: 0
-});
-const N = {
-    clamp: (min, value, max) => Math.min(Math.max(min, value), max),
-    rand: (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
-};
-const T = {
-    format: (date) => {
-        const hours = T.formatHours(date.getHours()), minutes = date.getMinutes(), seconds = date.getSeconds();
-        return `${hours}:${T.formatSegment(minutes)}`;
-    },
-    formatHours: (hours) => {
-        return hours % 12 === 0 ? 12 : hours % 12;
-    },
-    formatSegment: (segment) => {
-        return segment < 10 ? `0${segment}` : segment;
-    }
-};
-const LogInUtility = {
-    verify: async (pin) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (pin === Default.PIN) {
-                    resolve(true);
-                }
-                else {
-                    reject(`Invalid pin: ${pin}`);
-                }
-            }, N.rand(300, 700));
-        });
-    }
-};
-const useCurrentDateEffect = () => {
-    const [date, setDate] = React.useState(new Date());
-    React.useEffect(() => {
-        const interval = setInterval(() => {
-            const update = new Date();
-            if (update.getSeconds() !== date.getSeconds()) {
-                setDate(update);
-            }
-        }, 100);
-        return () => clearInterval(interval);
-    }, [date]);
-    return date;
-};
-const ScrollableComponent = (props) => {
-    const ref = React.useRef(null);
-    const [state, setStateTo] = React.useState({
-        grabbing: false,
-        position: defaultPosition()
-    });
-    const handleOnMouseDown = (e) => {
-        setStateTo(Object.assign(Object.assign({}, state), { grabbing: true, position: {
-                x: e.clientX,
-                left: ref.current.scrollLeft
-            } }));
-    };
-    const handleOnMouseMove = (e) => {
-        if (state.grabbing) {
-            const left = Math.max(0, state.position.left + (state.position.x - e.clientX));
-            ref.current.scrollLeft = left;
-        }
-    };
-    const handleOnMouseUp = () => {
-        if (state.grabbing) {
-            setStateTo(Object.assign(Object.assign({}, state), { grabbing: false }));
-        }
-    };
-    return (React.createElement("div", { ref: ref, className: classNames("scrollable-component", props.className), id: props.id, onMouseDown: handleOnMouseDown, onMouseMove: handleOnMouseMove, onMouseUp: handleOnMouseUp, onMouseLeave: handleOnMouseUp }, props.children));
-};
-const WeatherSnap = () => {
-    const [temperature] = React.useState(N.rand(65, 85));
-    return (React.createElement("span", { className: "weather" },
-        React.createElement("i", { className: "weather-type", className: "fa-duotone fa-sun" }),
-        React.createElement("span", { className: "weather-temperature-value" }, temperature),
-        React.createElement("span", { className: "weather-temperature-unit" }, "\u00B0F")));
-};
-const Reminder = () => {
-    return (React.createElement("div", { className: "reminder" },
-        React.createElement("div", { className: "reminder-icon" },
-            React.createElement("i", { className: "fa-regular fa-bell" })),
-        React.createElement("span", { className: "reminder-text" },
-            "Extra cool people meeting ",
-            React.createElement("span", { className: "reminder-time" }, "10AM"))));
-};
-const Time = () => {
-    const date = useCurrentDateEffect();
-    return (React.createElement("span", { className: "time" }, T.format(date)));
-};
-const Info = (props) => {
-    return (React.createElement("div", { id: props.id, className: "info" },
-        React.createElement(Time, null),
-        React.createElement(WeatherSnap, null)));
-};
-const PinDigit = (props) => {
-    const [hidden, setHiddenTo] = React.useState(false);
-    React.useEffect(() => {
-        if (props.value) {
-            const timeout = setTimeout(() => {
-                setHiddenTo(true);
-            }, 500);
-            return () => {
-                setHiddenTo(false);
-                clearTimeout(timeout);
-            };
-        }
-    }, [props.value]);
-    return (React.createElement("div", { className: classNames("app-pin-digit", { focused: props.focused, hidden }) },
-        React.createElement("span", { className: "app-pin-digit-value" }, props.value || "")));
-};
-const Pin = () => {
-    const { userStatus, setUserStatusTo } = React.useContext(AppContext);
-    const [pin, setPinTo] = React.useState("");
-    const ref = React.useRef(null);
-    React.useEffect(() => {
-        if (userStatus === UserStatus.LoggingIn || userStatus === UserStatus.LogInError) {
-            ref.current.focus();
-        }
-        else {
-            setPinTo("");
-        }
-    }, [userStatus]);
-    React.useEffect(() => {
-        if (pin.length === 4) {
-            const verify = async () => {
-                try {
-                    setUserStatusTo(UserStatus.VerifyingLogIn);
-                    if (await LogInUtility.verify(pin)) {
-                        setUserStatusTo(UserStatus.LoggedIn);
-                    }
-                }
-                catch (err) {
-                    console.error(err);
-                    setUserStatusTo(UserStatus.LogInError);
-                }
-            };
-            verify();
-        }
-        if (userStatus === UserStatus.LogInError) {
-            setUserStatusTo(UserStatus.LoggingIn);
-        }
-    }, [pin]);
-    const handleOnClick = () => {
-        ref.current.focus();
-    };
-    const handleOnCancel = () => {
-        setUserStatusTo(UserStatus.LoggedOut);
-    };
-    const handleOnChange = (e) => {
-        if (e.target.value.length <= 4) {
-            setPinTo(e.target.value.toString());
-        }
-    };
-    const getCancelText = () => {
-        return (React.createElement("span", { id: "app-pin-cancel-text", onClick: handleOnCancel }, "Cancel"));
-    };
-    const getErrorText = () => {
-        if (userStatus === UserStatus.LogInError) {
-            return (React.createElement("span", { id: "app-pin-error-text" }, "Invalid"));
-        }
-    };
-    return (React.createElement("div", { id: "app-pin-wrapper" },
-        React.createElement("input", { disabled: userStatus !== UserStatus.LoggingIn && userStatus !== UserStatus.LogInError, id: "app-pin-hidden-input", maxLength: 4, ref: ref, type: "number", value: pin, onChange: handleOnChange }),
-        React.createElement("div", { id: "app-pin", onClick: handleOnClick },
-            React.createElement(PinDigit, { focused: pin.length === 0, value: pin[0] }),
-            React.createElement(PinDigit, { focused: pin.length === 1, value: pin[1] }),
-            React.createElement(PinDigit, { focused: pin.length === 2, value: pin[2] }),
-            React.createElement(PinDigit, { focused: pin.length === 3, value: pin[3] })),
-        React.createElement("h3", { id: "app-pin-label" },
-            "Enter PIN (0000) ",
-            getErrorText(),
-            " ",
-            getCancelText())));
-};
-const MenuSection = (props) => {
-    const getContent = () => {
-        if (props.scrollable) {
-            return (React.createElement(ScrollableComponent, { className: "menu-section-content" }, props.children));
-        }
-        return (React.createElement("div", { className: "menu-section-content" }, props.children));
-    };
-    return (React.createElement("div", { id: props.id, className: "menu-section" },
-        React.createElement("div", { className: "menu-section-title" },
-            React.createElement("i", { className: props.icon }),
-            React.createElement("span", { className: "menu-section-title-text" }, props.title)),
-        getContent()));
-};
-const QuickNav = () => {
-    const getItems = () => {
-        return [{
-                id: 1,
-                label: "Weather"
-            }, {
-                id: 2,
-                label: "Food"
-            }, {
-                id: 3,
-                label: "Apps"
-            }, {
-                id: 4,
-                label: "Movies"
-            }].map((item) => {
-            return (React.createElement("div", { key: item.id, className: "quick-nav-item clear-button" },
-                React.createElement("span", { className: "quick-nav-item-label" }, item.label)));
-        });
-    };
-    return (React.createElement(ScrollableComponent, { id: "quick-nav" }, getItems()));
-};
 const Weather = () => {
     const getDays = () => {
         return [{
                 id: 1,
                 name: "Mon",
-                temperature: N.rand(60, 80),
+                temperature: N.rand(latitude,longitude),
                 weather: WeatherType.Sunny
             }, {
                 id: 2,
                 name: "Tues",
-                temperature: N.rand(60, 80),
+                temperature: N.rand(latitude,longitude),
                 weather: WeatherType.Sunny
             }, {
                 id: 3,
                 name: "Wed",
-                temperature: N.rand(60, 80),
+                temperature: N.rand(latitude,longitude),
                 weather: WeatherType.Cloudy
             }, {
                 id: 4,
                 name: "Thurs",
-                temperature: N.rand(60, 80),
+                temperature: N.rand(latitude,longitude),
                 weather: WeatherType.Rainy
             }, {
                 id: 5,
                 name: "Fri",
-                temperature: N.rand(60, 80),
+                temperature: N.rand(latitude,longitude),
                 weather: WeatherType.Stormy
             }, {
                 id: 6,
                 name: "Sat",
-                temperature: N.rand(60, 80),
+                temperature: N.rand(latitude,longitude),
                 weather: WeatherType.Sunny
             }, {
                 id: 7,
                 name: "Sun",
-                temperature: N.rand(60, 80),
+                temperature: N.rand(latitude,longitude),
                 weather: WeatherType.Cloudy
             }].map((day) => {
             const getIcon = () => {
@@ -282,7 +52,7 @@ const Weather = () => {
                 React.createElement("div", { className: "day-card-content" },
                     React.createElement("span", { className: "day-weather-temperature" },
                         day.temperature,
-                        React.createElement("span", { className: "day-weather-temperature-unit" }, "\u00B0F")),
+                        React.createElement("span", { className: "day-weather-temperature-unit" }, "\u00B0C")),
                     React.createElement("i", { className: classNames("day-weather-icon", getIcon(), day.weather.toLowerCase()) }),
                     React.createElement("span", { className: "day-name" }, day.name))));
         });
